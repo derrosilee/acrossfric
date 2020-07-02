1096b1024b7520cace9fc660e97631e1228243e2
lee <54778660+derrosilee@users.noreply.github.com>
2020-07-02 19:31:02 +0300

    made admin site Basic Registrations

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Clients, Claims
 
 # Register your models here.
+
+class  ClientsAdmin(admin.ModelAdmin):
+    list_display = ("Name", 'Class_insured', 'Car_plate',  'Insuerer', "Policy_no", "Date_insured", "Date_expiry", "Rate", "Sum_insured", "Annual_premium", "Commision") 
+    search_fields = ("Name", "Insuerer")
+
+admin.site.site_header = "Accros Africa Insurance Agency CMS"
+admin.site.register(Clients, ClientsAdmin)
+
+class ClaimsAdmin(admin.ModelAdmin):
+    list_display = ("Name", "Class_insurance", "Policy_no", "Claim_no", "Date_Accident", "Place", "Explanation")
+    search_fields = ("Name", "Claim_no")
+
+admin.site.register(Claims, ClaimsAdmin)
